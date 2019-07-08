7544207c2f527b6ab6bd8489728d08b7b3231a5d
anragsor <anragsor@gmail.com>
2019-07-08 16:27:04 +0400

    Всякое!

--- javaTest.java
+++ javaTest.java
@@ -1,5 +1,12 @@
 class javaTest{
 	public static void main (String [] args){
 		System.out.println("Hello,blyat!!!111))00");
+	javaOhh Ah = new javaOhh();
+	System.out.println(Ah.ohohoh());
+	}
+}
+class javaOhh {
+	String ohohoh (){
+		return "Fuck you!";
 	}
 }
